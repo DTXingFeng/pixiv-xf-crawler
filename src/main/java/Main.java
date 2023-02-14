7156f83634527da22c43bb6991d2dac49088bdb1
Administrator <2695570953@qq.com>
2023-02-14 14:54:00 +0800

    1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        setProperty("127.0.0.1","7890");
+    }
 
+    /**
+     * 设置代理
+     */
+    public static void setProperty(String Host,String Port){
+        System.setProperty("http.proxyHost", Host);
+        System.setProperty("http.proxyPort", Port);
+
+        // 对https也开启代理
+        System.setProperty("https.proxyHost", Host);
+        System.setProperty("https.proxyPort", Port);
+    }
 }
