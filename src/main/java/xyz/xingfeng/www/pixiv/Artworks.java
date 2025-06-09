f02ea089efa20c707abf8dcb24b2408dac9a9ab2
DTXingFeng <2695570953@qq.com>
2025-06-09 22:02:48 +0800

    1.1

--- src/main/java/xyz/xingfeng/www/pixiv/Artworks.java
+++ src/main/java/xyz/xingfeng/www/pixiv/Artworks.java
@@ -13,14 +13,15 @@ public class Artworks {
 
     private ArrayList<String> downloadUrls = new ArrayList<>();
 
-    private String url = "https://www.pixiv.net/artworks/";
+    private String url = "https://www.pixiv.net/ajax/illust/%s?lang=zh";
     private int like = 0;
     private int minLike = 0;
     String id = "0";
-    public Artworks(String id){
+
+    public Artworks(String id) {
         this.id = id;
         //构建url
-        url+=id;
+        url = String.format(url, id);
         //请求页面
         GetArtworksHtml getArtworksHtml = new GetArtworksHtml(url);
         //获得网页信息
@@ -40,43 +41,32 @@ public class Artworks {
         //构建json
         JSONObject jsonObject;
         int x = 0;
-        while (true){
-            if (x > 5){
-                throw new RuntimeException();
-            }
-            //解析网页信息
-            Document doc = Jsoup.parse(html);
-            //筛选
-            Elements select = doc.select("meta[name=\"preload-data\"]");
-            String content = select.attr("content");
-            x++;
-            try {
-                jsonObject = new JSONObject(content);
-                break;
-            } catch (Exception e) {
-                System.out.println();
-            }
+        //解析网页信息
+        JSONObject json = new JSONObject(html);
+        if (json.getBoolean("error")) {
+            System.out.println("获取数据失败，可能是id错误");
+            return;
         }
         //获得点赞数
-        like = jsonObject.getJSONObject("illust").getJSONObject(id).getInt("bookmarkCount");
-        if (like<minLike){
+        int like = json.getJSONObject("body").getInt("bookmarkCount");
+        if (like < minLike) {
             return;
         }
         //获得下载链接
-        GetArtworksHtml getArtworksHtml1 = new GetArtworksHtml("https://www.pixiv.net/ajax/illust/"+id+"/pages?lang=zh");
+        GetArtworksHtml getArtworksHtml1 = new GetArtworksHtml("https://www.pixiv.net/ajax/illust/" + id + "/pages?lang=zh");
         JSONObject dowUrl = new JSONObject(getArtworksHtml1.getHtml());
         JSONArray body = dowUrl.getJSONArray("body");
-        for (int i = 0; i<body.length(); i++){
+        for (int i = 0; i < body.length(); i++) {
             String downloadUrl = body.getJSONObject(i).getJSONObject("urls").getString("original");
             downloadUrls.add(downloadUrl);
         }
     }
 
-    public void Download(){
-        if (downloadUrls.equals(new ArrayList<>())){
+    public void Download() {
+        if (downloadUrls.equals(new ArrayList<>())) {
             return;
         }
-        for (String url : downloadUrls){
+        for (String url : downloadUrls) {
             new DownloadImg(url);
         }
     }
